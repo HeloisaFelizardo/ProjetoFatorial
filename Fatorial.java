c5f967db417c038fb98ec9012f459a11acf89632
Heloisa Felizardo <helocmp@yahoo.com.br>
2022-06-10 11:11:26 -0300

    Primeiro commit  arquivos Fatorial e TesteFatorial .java

---
+++ Fatorial.java
@@ -0,0 +1,13 @@
+public class Fatorial {
+	//Versão iterativa do calculo do fatorial
+	public int calculaFatorial (int n) throws IllegalArgumentException {
+		if(n < 0)
+			throw new IllegalArgumentException ("não existe fatorial para numeros negativos");
+		int resultado = 1;
+		if (n!=0){
+			for (int i = 2; i <= n; i++)
+			resultado*= i;
+		}
+		return resultado;
+	}
+}
---
+++ TesteFatorial.java
@@ -0,0 +1,7 @@
+class TesteFatorial {
+	public static void main(String[] args){
+		Fatorial fat = new Fatorial();
+		int resultado = fat.calculaFatorial(0);
+		System.out.println(resultado);
+	}
+}
