5709ea6945f1b3bd4bac7ceae3664f49ab0a6cea
Heloisa Felizardo <helocmp@yahoo.com.br>
2022-06-12 19:53:49 -0300

    corrigindo erros no código

--- FatorialRecursivo.java
+++ FatorialRecursivo.java
@@ -1,10 +1,10 @@
-puclic class FatorialRecursivo{
+public class FatorialRecursivo{
     public static int calcularFatorialRecursivo (int n) throws IllegalArgumentException {
 	if (n == 0)
 	   return 1;
-	return n * calcularFatorialRecursivo(n-1)
+	return n * calcularFatorialRecursivo(n-1);
     }
-    public static void main(String[]args{
+    public static void main(String[]args){
 	System.out.println(calcularFatorialRecursivo(5));
     }
 }
